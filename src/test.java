712c6475dd81ab9ce55b7881e77810fa245fe7bc
andreaDiClaudio <andreadicla@gmail.com>
2021-09-13 09:57:52 +0200

    Second edit

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("OK");  
         // This is an edit example
+        //THIS IS  A SECOND EXAMPLE
     }
 }
